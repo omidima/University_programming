b054e62ea57572f73d8255be20b0043dc916259c
omidima <omid814445@gmail.com>
2021-03-03 01:44:18 -0500

    omidima

---
+++ session-4/index.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class index {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(5);
+        list.add(1);
+        list.add(15);
+        System.out.println(list);
+    }
+}
